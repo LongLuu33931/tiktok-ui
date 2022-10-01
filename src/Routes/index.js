0e4a54be06b2bc04edc91763928bfd3b71e34ce3
LongLuu <64773511+LongLuu33931@users.noreply.github.com>
2022-10-01 17:02:14 +0700

    Logic Header #2

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -8,11 +8,11 @@ import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
 //public Route
 const publicRoutes = [
-  { path: '/', component: Home },
-  { path: '/following', component: Following },
-  { path: '/profile', component: Profile },
-  { path: '/upload', component: Upload, layout: HeaderOnly },
-  { path: '/search', component: Search, layout: null },
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/@:nickname', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
 ];
 
 const privateRoutes = [];
--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,26 +1,23 @@
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import Image from '~/components/Image';
 import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
-            <Image
-                className={cx('avatar')}
-                src="https://scontent.fsgn5-11.fna.fbcdn.net/v/t39.30808-6/269017650_3116251511993313_7016843383586218216_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=tM21Q4K6hQQAX8rnNdH&_nc_ht=scontent.fsgn5-11.fna&oh=00_AT8x1izQHzsnja_LWcdEBybaDqc3Ki2b_64x89211mPOdQ&oe=633B16F2"
-                alt="avatar"
-            />
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.full_name} />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    <span>Long Lưu</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
                 </h4>
-                <span className={cx('username')}>longluuneeee</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
